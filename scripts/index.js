f84292594295c0d5c9bd6f9bd31e472489a8eba6
Oleg Zharov <levineye@gmail.com>
2020-08-07 19:43:03 +0300

    feat: добавить бургер-меню

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,16 @@
-const popupLink = document.querySelector('#popup-link')
+const headerMenuList = document.querySelector('.header__lang-block');
+const popupLink = headerMenuList.querySelector('#popup-link')
+const headerMenuButton = document.querySelector('.header__button-menu');
 const popup = document.querySelector('.popup');
 const popupСloseButton = popup.querySelector('.popup__close-button_container');
 const containerCloseButton = popup.querySelector('.popup__close-button_image-container');
 const illustrationLink = popup.querySelector('.popup__link');
 const popupImageContainer = popup.querySelector('.popup__image-container');
+
+function usingMenu() {
+	headerMenuButton.classList.toggle('header__button-menu_opened');
+	headerMenuList.classList.toggle('header__lang-block_opened')
+}
 
 function openPopup(evt) {
 	evt.preventDefault();
@@ -26,6 +33,7 @@ function closePopupIllustration() {
 	illustrationLink.classList.add('popup__link_active');
 }
 
+headerMenuButton.addEventListener('click', usingMenu);
 popupLink.addEventListener('click', openPopup);
 popupСloseButton.addEventListener('click', closePopup);
 illustrationLink.addEventListener('click', openPopupIllustration);
